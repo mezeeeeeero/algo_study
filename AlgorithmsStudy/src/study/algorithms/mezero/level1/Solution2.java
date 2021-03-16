e3d9915b743095ab04d75e5c93d986562790a4e6
mezeeeeeero <git.mezero@gmail.com>
2021-03-16 13:13:16 +0900

    2st Algorithm

---
+++ AlgorithmsStudy/src/study/algorithms/mezero/level1/Solution2.java
@@ -0,0 +1,13 @@
+package study.algorithms.mezero.level1;
+
+public class Solution2 {
+
+    public int solution1(int[] a, int[] b) {
+        int answer = 0;
+        for(int i =0; i < a.length; i++){
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+
+}
