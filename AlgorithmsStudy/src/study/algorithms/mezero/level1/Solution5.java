060cc332e2238d82f51d19719ffac5a5680067b7
mezeeeeeero <git.mezero@gmail.com>
2021-03-16 15:12:48 +0900

    5th Algorithm > make Prime

---
+++ AlgorithmsStudy/src/study/algorithms/mezero/level1/Solution5.java
@@ -0,0 +1,32 @@
+package study.algorithms.mezero.level1;
+
+import java.util.Arrays;
+
+public class Solution5 {
+    public int solution1(int[] nums) {
+        int sum = 0;
+        int answer = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                for(int k = j+1; k < nums.length; k++){
+                    sum = nums[i] + nums[j] + nums[k];
+                    if(isPrime(sum)){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static boolean isPrime(int sum){
+        boolean isPrime = true;
+        for(int i = 2; i < sum; i++){
+            if(sum % i == 0){
+                isPrime = false;
+            }
+        }
+        return isPrime;
+    }
+
+}
