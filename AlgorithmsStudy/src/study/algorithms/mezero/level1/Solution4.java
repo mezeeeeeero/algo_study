b207087cc45a0c538fd5136dc83992fca35a253f
mezeeeeeero <git.mezero@gmail.com>
2021-03-16 14:32:58 +0900

    4st Algorithm

---
+++ AlgorithmsStudy/src/study/algorithms/mezero/level1/Solution4.java
@@ -0,0 +1,25 @@
+package study.algorithms.mezero.level1;
+
+import java.util.Arrays;
+
+public class Solution4 {
+
+    /**
+     * 배열 d를 오름차순으로 정렬하여 budget에서 그 값들을 빼다가 budget이 0이 되면 그 전 까지의 지원부서 수를 return
+     */
+    public int solution1(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+
+        for(int i = 0; i < d.length; i++){
+            budget = budget - d[i];
+            if(budget < 0){
+                break;
+            }
+            answer++;
+        }
+
+        System.out.println(answer);
+        return answer;
+    }
+}
